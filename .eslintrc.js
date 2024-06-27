acf04a568e16bcd5ee65b7c7b7c65d14db6c6fbe
Yara Tavares <yaracriistavares@gmail.com>
2024-06-27 18:19:49 -0300

    fix: build

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+	root: true,
+	env: {
+		node: true,
+	},
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier',
+	],
+	rules: {
+		'@typescript-eslint/no-var-requires': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-unused-vars': 'off',
+	},
+};
